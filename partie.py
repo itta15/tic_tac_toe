dc45f018e8e4e2525218f6cd6e1d6bbc67004c87
itta15 <145795212+itta15@users.noreply.github.com>
2023-11-24 16:32:17 +0100

    Add files via upload

--- partie.py
+++ partie.py
@@ -1,17 +1,22 @@
-import numpy as np
 import pygame
+from case import Case
 
 class Partie(pygame.sprite.Sprite):
 
     def __init__ (self):
         super().__init__()
         self.tab = [[0 for _ in range(3)] for _ in range (3)]
+        self.all_case_comp = pygame.sprite.Group()
         self.fin = False
-        self.turn = 1
-        self.fond = pygame.image.load("assets/tab.jpg")
-        self.rect_fond = self.fond.get_rect()
-        self.rect_fond.x = (1080 - 600)/2
-        self.rect_fond.y = (720-600)/2
+        self.player = 1
+        self.image = pygame.image.load("assets/tab.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.x = 240
+        self.rect.y = 60
 
-
+        
+    #turn aura d'autre argument pour la case designe
+    def turn (self,i,j):
+        self.all_case_comp.add(Case(self.player,i,j))
+        self.player = 1 if self.player ==2 else 2 #permet de changer de joueur apres avoir clique
 
